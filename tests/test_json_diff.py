d50c3ac7a9a9297abc5051b6178c0cee14d4f10a
dayan holguin marin <113401319+dayanholguinmarin@users.noreply.github.com>
2025-12-16 05:20:38 -0500

    11,1
    11.1

--- tests/test_json_diff.py
+++ tests/test_json_diff.py
@@ -5,9 +5,44 @@ def test_diff_files_json():
     file1 = 'tests/fixtures/file1.json'
     file2 = 'tests/fixtures/file2.json'
     expected = """{
-      - proxy: 123.234.53.22
-      - timeout: 50
-      + timeout: 20
-      + verbose: true
+      common: {
+        + follow: false
+        - setting2: 200
+        - setting3: true
+        + setting3: null
+        + setting4: blah blah
+        + setting5: {
+              key5: value5
+          }
+          setting6: {
+              doge: {
+                - wow: 
+                + wow: so much
+              }
+            + ops: vops
+          }
+      }
+      group1: {
+        - baz: bas
+        + baz: bars
+        - nest: {
+              key: value
+          }
+        + nest: str
+      }
+    - group2: {
+          abc: 12345
+          deep: {
+              id: 45
+          }
+      }
+    + group3: {
+          deep: {
+              id: {
+                  number: 45
+              }
+          }
+          fee: 100500
+      }
     }"""
     assert generate_diff(file1, file2) == expected
