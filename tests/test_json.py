14bd3a360ea7ab6c5cd78815596e2a6ba11d0eda
dayan holguin marin <113401319+dayanholguinmarin@users.noreply.github.com>
2026-01-05 19:23:05 -0500

    correciones ident
    ident

--- tests/test_json.py
+++ tests/test_json.py
@@ -3,18 +3,18 @@ import pathlib
 
 
 def read_expected(filename):
-    path = pathlib.Path('tests/fixtures') / filename
-    return path.read_text().strip()
+  path = pathlib.Path('tests/fixtures') / filename
+  return path.read_text().strip()
 
 
 def test_diff_files_json():
-    file1 = 'tests/fixtures/file1.json'
-    file2 = 'tests/fixtures/file2.json'
-    expected = read_expected('expected_diff.txt')
-    assert generate_diff(file1, file2) == expected
+  file1 = 'tests/fixtures/file1.json'
+  file2 = 'tests/fixtures/file2.json'
+  expected = read_expected('expected_diff.txt')
+  assert generate_diff(file1, file2) == expected
 
 
 def test_equal_files_json():
-    file1 = 'tests/fixtures/file1.json'
-    file2 = 'tests/fixtures/file1.json'
-    assert generate_diff(file1, file2) == ""
+  file1 = 'tests/fixtures/file1.json'
+  file2 = 'tests/fixtures/file1.json'
+  assert generate_diff(file1, file2) == ""
