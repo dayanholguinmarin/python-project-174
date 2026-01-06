522090ddeee52917018e9bc4ca391c65a4bc7808
dayan holguin marin <113401319+dayanholguinmarin@users.noreply.github.com>
2026-01-05 20:42:50 -0500

    flat
    flat

--- tests/test_yaml_diff.py
+++ tests/test_yaml_diff.py
@@ -5,12 +5,12 @@ def test_flat_yaml_diff():
     file1 = "tests/fixtures/file1.yml"
     file2 = "tests/fixtures/file2.yml"
     expected = """{
-    - follow: false
-      host: hexlet.io
-    - proxy: 123.234.53.22
-    - timeout: 50
-    + timeout: 20
-    + verbose: true
+      - follow: false
+        host: hexlet.io
+      - proxy: 123.234.53.22
+      - timeout: 50
+      + timeout: 20
+      + verbose: true
     }"""
     result = generate_diff(file1, file2)
     assert result == expected
