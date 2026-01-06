38529c998a73b528a4f7203bcc17e7cbb0b0658a
dayan holguin marin <113401319+dayanholguinmarin@users.noreply.github.com>
2026-01-05 19:34:41 -0500

    hole
    hole

--- tests/test_yaml_diff.py
+++ tests/test_yaml_diff.py
@@ -2,21 +2,21 @@ from gendiff import generate_diff
 
 
 def test_flat_yaml_diff():
-  file1 = "tests/fixtures/file1.yml"
-  file2 = "tests/fixtures/file2.yml"
-  expected = """{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
+    file1 = "tests/fixtures/file1.yml"
+    file2 = "tests/fixtures/file2.yml"
+    expected = """{
+    - follow: false
+      host: hexlet.io
+    - proxy: 123.234.53.22
+    - timeout: 50
+    + timeout: 20
+    + verbose: true
   }"""
-  result = generate_diff(file1, file2)
-  assert result == expected
+    result = generate_diff(file1, file2)
+    assert result == expected
 
 
 def test_equal_yaml_files():
-  file1 = "tests/fixtures/file1.yml"
-  file2 = "tests/fixtures/file1.yml"
-  assert generate_diff(file1, file2) == ""
+    file1 = "tests/fixtures/file1.yml"
+    file2 = "tests/fixtures/file1.yml"
+    assert generate_diff(file1, file2) == ""
