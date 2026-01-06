c4a41201e2675a4e7a84f87a5539db362015c39d
dayan holguin marin <113401319+dayanholguinmarin@users.noreply.github.com>
2026-01-05 19:17:12 -0500

    ultimo push
    ultimo push

--- tests/test_json.py
+++ tests/test_json.py
@@ -1,15 +1,17 @@
 from gendiff import generate_diff
 import pathlib
 
+
 def read_expected(filename):
-  path = pathlib.Path('tests/fixtures') / filename
-  return path.read_text().strip()
+    path = pathlib.Path('tests/fixtures') / filename
+    return path.read_text().strip()
+
 
 def test_diff_files_json():
-  file1 = 'tests/fixtures/file1.json'
-  file2 = 'tests/fixtures/file2.json'
-  expected = read_expected('expected_diff.txt')
-  assert generate_diff(file1, file2) == expected
+    file1 = 'tests/fixtures/file1.json'
+    file2 = 'tests/fixtures/file2.json'
+    expected = read_expected('expected_diff.txt')
+    assert generate_diff(file1, file2) == expected
 
 
 def test_equal_files_json():
--- tests/test_yaml_diff.py
+++ tests/test_yaml_diff.py
@@ -3,9 +3,9 @@ from gendiff import generate_diff
 
 def test_flat_yaml_diff():
 
-  file1 = "tests/fixtures/file1.yml"
-  file2 = "tests/fixtures/file2.yml"
-  expected = """{
+    file1 = "tests/fixtures/file1.yml"
+    file2 = "tests/fixtures/file2.yml"
+    expected = """{
   - follow: false
     host: hexlet.io
   - proxy: 123.234.53.22
@@ -13,12 +13,11 @@ def test_flat_yaml_diff():
   + timeout: 20
   + verbose: true
 }"""
-  result = generate_diff(file1, file2)
-  assert result == expected
+    result = generate_diff(file1, file2)
+    assert result == expected
 
 
 def test_equal_yaml_files():
-  file1 = "tests/fixtures/file1.yml"
-  file2 = "tests/fixtures/file1.yml"
-  assert generate_diff(file1, file2) == ""
-  
+    file1 = "tests/fixtures/file1.yml"
+    file2 = "tests/fixtures/file1.yml"
+    assert generate_diff(file1, file2) == ""
